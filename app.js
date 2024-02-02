45302f7ed76afa8af1e2f171b0a8c7ed9e008d25
Juan Ignacio Grodz <jigrodz@gmail.com>
2024-02-02 12:05:32 -0300

    Primera modificacion al html con queryselector

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Juegro del numero secreto"
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Coloca un numero del 1 al 10"
