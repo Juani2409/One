30f45266f53766cfc5d70c49be06d664f81b356b
Juan Ignacio Grodz <jigrodz@gmail.com>
2024-02-02 15:03:30 -0300

    Funcion math.random

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto)
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function intentoDeUsuario(){
     alert("click desde el boton")
+    return;
+}
+
+function generarNumeroSecreto() {
+  
+    return Math.floor(Math.random()*10)+1;
+    
 }
 
 
